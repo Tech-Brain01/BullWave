84703f7539605a5ce65eac42913229eb8fcebf81
Tech-Brain01 <tomar.amrendera@outlook.com>
2025-07-18 15:03:57 +0530

    created navbar , homepage , footer

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/HomePage/Hero'
+import Features from '../components/HomePage/Features'
+import Preview from '../components/HomePage/Preview'
+
+const Home = () => {
+  return (
+    <>
+       <Hero />
+      <Features />
+      <Preview />
+    </>
+  )
+}
+
+export default Home
